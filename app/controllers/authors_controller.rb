bfcebf9f3ae665de43048368733b05392c705017
mark mwendwa <mwendwamark310@gmail.com>
2023-06-21 20:58:43 +0300

    completed code along

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,23 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_not_found_response
+
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
   private
-  
+
   def author_params
     params.permit(:email, :name)
   end
-  
+
+  def render_not_found_response(invalid)
+   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
